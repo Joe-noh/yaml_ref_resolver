10450f0c250d1a8c17d349ad56794d1f834e5402
Joe Honzawa <goflb.jh@gmail.com>
2017-01-26 23:11:55 +0900

    Classify YamlRefResolver (#2)
    * make YamlRefResolver class
    
    * implement parsing single yaml file
    
    just passthrough into YAML.load_file
    
    * walk yaml recursively but functionlity is not changed

--- lib/yaml_ref_resolver.rb
+++ lib/yaml_ref_resolver.rb
@@ -1,5 +1,27 @@
 require "yaml_ref_resolver/version"
+require "yaml"
 
-module YamlRefResolver
-  # Your code goes here...
+class YamlRefResolver
+  def initialize
+  end
+
+  def resolve(path)
+    resolve_refs(YAML.load_file path)
+  end
+
+  private
+
+  def resolve_refs(obj)
+    return resolve_hash(obj)  if obj.is_a? Hash
+    return resolve_array(obj) if obj.is_a? Array
+    return obj
+  end
+
+  def resolve_hash(hash)
+    hash.map {|k, v| [k, resolve_refs(v)] }.to_h
+  end
+
+  def resolve_array(array)
+    array.map {|e| resolve_refs(e) }
+  end
 end
