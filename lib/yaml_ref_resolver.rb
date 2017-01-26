f1048af9e4c3708e23c987cff8180a7a76d9ad14
Joe-noh <goflb.jh@gmail.com>
2017-01-26 23:03:01 +0900

    walk yaml recursively but functionlity is not changed

--- lib/yaml_ref_resolver.rb
+++ lib/yaml_ref_resolver.rb
@@ -6,6 +6,22 @@ class YamlRefResolver
   end
 
   def resolve(path)
-    YAML.load_file(path)
+    resolve_refs(YAML.load_file path)
+  end
+
+  private
+
+  def resolve_refs(obj)
+    return resolve_hash(obj)  if obj.is_a? Hash
+    return resolve_array(obj) if obj.is_a? Array
+    return obj
+  end
+
+  def resolve_hash(hash)
+    hash.map {|k, v| [k, resolve_refs(v)] }.to_h
+  end
+
+  def resolve_array(array)
+    array.map {|e| resolve_refs(e) }
   end
 end
