b6f6434812c47f77b98b996ab4c2d5f8bf11a59d
Joe Honzawa <goflb.jh@gmail.com>
2017-01-27 22:30:33 +0900

    Basic Implementation (#3)
    * add test for multi file resolving
    
    * support yamls which containing objects
    
    * test resolving yaml having array
    
    * test deep referencing

--- test/yaml_ref_resolver_test.rb
+++ test/yaml_ref_resolver_test.rb
@@ -1,11 +1,36 @@
 require 'test_helper'
 
 class YamlRefResolverTest < Minitest::Test
+  def setup
+    @resolver = YamlRefResolver.new
+  end
+
   def test_resolve_single_yaml
     path = File.join(File.dirname(__FILE__), *%w[yamls single.yaml])
-    resolver = YamlRefResolver.new
-    yaml = resolver.resolve(path)
+    yaml = @resolver.resolve(path)
 
     assert_equal yaml, YAML.load_file(path)
+  end
+
+  def test_resolve_yaml_containing_object
+    path = File.join(File.dirname(__FILE__), *%w[yamls object index.yaml])
+    yaml = @resolver.resolve(path)
+
+    assert_equal yaml['paths']['/products']['post']['tags'].first, 'product'
+  end
+
+  def test_resolve_yaml_containing_array
+    path = File.join(File.dirname(__FILE__), *%w[yamls array index.yaml])
+    yaml = @resolver.resolve(path)
+
+    assert_equal yaml['produces'][0], 'application/json'
+    assert_equal yaml['produces'][1], 'text/html'
+  end
+
+  def test_resolve_yaml_containing_deep_refs
+    path = File.join(File.dirname(__FILE__), *%w[yamls deep index.yaml])
+    yaml = @resolver.resolve(path)
+
+    assert_equal yaml['paths']['/products']['post']['responses'][201]['description'], 'Successfully created.'
   end
 end
