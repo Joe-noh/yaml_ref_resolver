7d5e13b19b345fbc6d28a82bc0c042fae1b6c64a
Joe-noh <goflb.jh@gmail.com>
2017-01-28 16:51:24 +0900

    make cli support custom key

--- lib/yaml_ref_resolver/cli.rb
+++ lib/yaml_ref_resolver/cli.rb
@@ -15,7 +15,7 @@ class YamlRefResolver
 
       validate_input_path
 
-      resolver = YamlRefResolver.new
+      resolver = YamlRefResolver.new(key: @key)
       yaml = resolver.resolve(@input)
 
       $stdout.write(yaml.to_yaml)
@@ -33,16 +33,20 @@ class YamlRefResolver
         @input = path
       end
 
-      def validate_input_path
-        unless @input
-          puts "please specify input yaml with --input option"
-          exit 1
-        end
+      @opt.on('-k key', '--key', 'key to be resolved. $ref by default') do |key|
+        @key = key
+      end
+    end
 
-        unless File.exists?(@input)
-          puts "#{@input} not found"
-          exit 1
-        end
+    def validate_input_path
+      unless @input
+        puts "please specify input yaml with --input option"
+        exit 1
+      end
+
+      unless File.exists?(@input)
+        puts "#{@input} not found"
+        exit 1
       end
     end
   end
