568920d90c5b7adaf5cb75e84cfb91d8e274b967
Joe Honzawa <goflb.jh@gmail.com>
2017-01-28 16:47:22 +0900

    CLI (#9)
    * create cli
    
    * dont need output option
    
    use > instead
    
    * abort if input file not found
    
    * validate given input path

---
+++ lib/yaml_ref_resolver/cli.rb
@@ -0,0 +1,49 @@
+require "yaml_ref_resolver"
+require "optparse"
+
+class YamlRefResolver
+  class CLI
+    def initialize
+      @opt = OptionParser.new
+      @input = nil
+
+      define_options
+    end
+
+    def run(argv)
+      @opt.parse!(argv)
+
+      validate_input_path
+
+      resolver = YamlRefResolver.new
+      yaml = resolver.resolve(@input)
+
+      $stdout.write(yaml.to_yaml)
+    end
+
+    private
+
+    def define_options
+      @opt.on('-v', '--version', 'show version number.') do
+        puts "YamlRefResolver v#{YamlRefResolver::VERSION}"
+        exit
+      end
+
+      @opt.on('-i path', '--input', 'entry point path') do |path|
+        @input = path
+      end
+
+      def validate_input_path
+        unless @input
+          puts "please specify input yaml with --input option"
+          exit 1
+        end
+
+        unless File.exists?(@input)
+          puts "#{@input} not found"
+          exit 1
+        end
+      end
+    end
+  end
+end
