3d7f335c5a992d891dc8f733116e29c3910e418c
Joe-noh <goflb.jh@gmail.com>
2017-03-22 00:09:16 +0900

    rename

--- lib/yaml_ref_resolver.rb
+++ lib/yaml_ref_resolver.rb
@@ -4,34 +4,34 @@ require "yaml_ref_resolver/yaml"
 class YamlRefResolver
   def initialize(opts = {})
     @key = opts[:key] || '$ref'
-    @yaml = {}
+    @map = {}
   end
 
   def resolve(path)
     entry_point = File.expand_path(path)
-    preload_ref_yamls(entry_point)
+    preload(entry_point)
 
-    resolve_refs(@yaml[entry_point].content, entry_point)
+    resolve_refs(@map[entry_point].content, entry_point)
   end
 
   def reload(path)
-    @yaml.delete(path)
-    preload_ref_yamls(path)
+    @map.delete(path)
+    preload(path)
   end
 
   def files
-    @yaml.keys
+    @map.keys
   end
 
   private
 
-  def preload_ref_yamls(abs_path)
-    return if @yaml.has_key?(abs_path)
+  def preload(abs_path)
+    return if @map.has_key?(abs_path)
 
-    @yaml[abs_path] = Yaml.new(path: abs_path, key: @key)
-    @yaml[abs_path].refs.each do |ref_target|
+    @map[abs_path] = Yaml.new(path: abs_path, key: @key)
+    @map[abs_path].refs.each do |ref_target|
       rel_path, pos = parse_ref(ref_target)
-      preload_ref_yamls(File.expand_path(rel_path, File.dirname(abs_path)))
+      preload(File.expand_path(rel_path, File.dirname(abs_path)))
     end
   end
 
@@ -49,9 +49,9 @@ class YamlRefResolver
         pos_keys = pos.split('/').reject {|s| s == "" }
 
         if pos_keys.size == 0
-          resolve_refs(@yaml[ref_path].content, ref_path)
+          resolve_refs(@map[ref_path].content, ref_path)
         else
-          resolve_refs(@yaml[ref_path].content.dig(*pos_keys), ref_path)
+          resolve_refs(@map[ref_path].content.dig(*pos_keys), ref_path)
         end
       else
         Hash[key, resolve_refs(val, referrer)]
