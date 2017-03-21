04864851ae363d576a1a6f0f8d2e3c1f4006b40a
Joe-noh <goflb.jh@gmail.com>
2017-03-21 23:59:54 +0900

    move ref finding login to Yaml class

--- lib/yaml_ref_resolver.rb
+++ lib/yaml_ref_resolver.rb
@@ -1,5 +1,5 @@
 require "yaml_ref_resolver/version"
-require "yaml"
+require "yaml_ref_resolver/yaml"
 
 class YamlRefResolver
   def initialize(opts = {})
@@ -11,7 +11,7 @@ class YamlRefResolver
     entry_point = File.expand_path(path)
     preload_ref_yamls(entry_point)
 
-    resolve_refs(@yaml[entry_point], entry_point)
+    resolve_refs(@yaml[entry_point].content, entry_point)
   end
 
   def reload(path)
@@ -28,29 +28,11 @@ class YamlRefResolver
   def preload_ref_yamls(abs_path)
     return if @yaml.has_key?(abs_path)
 
-    @yaml[abs_path] = YAML.load_file(abs_path)
-    find_refs(@yaml[abs_path]).each do |ref_target|
+    @yaml[abs_path] = Yaml.new(path: abs_path, key: @key)
+    @yaml[abs_path].refs.each do |ref_target|
       rel_path, pos = parse_ref(ref_target)
       preload_ref_yamls(File.expand_path(rel_path, File.dirname(abs_path)))
     end
-  end
-
-  def find_refs(obj)
-    do_find_refs(obj).flatten.compact
-  end
-
-  def do_find_refs(obj)
-    return do_find_refs_hash(obj)  if obj.is_a? Hash
-    return do_find_refs_array(obj) if obj.is_a? Array
-    return []
-  end
-
-  def do_find_refs_hash(hash)
-    hash.inject([]) {|acc, (key, val)| acc << (key == @key ? val : do_find_refs(val)) }
-  end
-
-  def do_find_refs_array(array)
-    array.map {|e| find_refs(e) }
   end
 
   def resolve_refs(obj, referrer)
@@ -67,9 +49,9 @@ class YamlRefResolver
         pos_keys = pos.split('/').reject {|s| s == "" }
 
         if pos_keys.size == 0
-          resolve_refs(@yaml[ref_path], ref_path)
+          resolve_refs(@yaml[ref_path].content, ref_path)
         else
-          resolve_refs(@yaml[ref_path].dig(*pos_keys), ref_path)
+          resolve_refs(@yaml[ref_path].content.dig(*pos_keys), ref_path)
         end
       else
         Hash[key, resolve_refs(val, referrer)]
